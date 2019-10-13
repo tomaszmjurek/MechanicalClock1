09633b144e9eba800558431166d030fe82f2459f
arabello7 <tomekj97@gmail.com>
2019-10-13 19:46:13 +0200

    Sphere enlarged

--- Zegar/Models/Backstage/Backstage.cpp
+++ Zegar/Models/Backstage/Backstage.cpp
@@ -26,6 +26,7 @@ void Backstage::drawObject(glm::mat4 mP, glm::mat4 mV) {
 	glm::mat4 mM = glm::mat4(1.0f);
 	mM = glm::translate(mM, getPosition());
 	mM = glm::translate(mM, glm::vec3(0, 0, 0));
+	mM = glm::scale(mM, glm::vec3(1.4, 1.4, 1.4)); //powiekszenie
 	glm::mat4 rotZ = glm::rotate(glm::mat4(1.0f), 3.14f * 90 / 180, glm::vec3(0, 0, 1));
 	glm::mat4 rotY = glm::rotate(glm::mat4(1.0f), 3.14f * 90 / 180, glm::vec3(0, 1, 0));
 
